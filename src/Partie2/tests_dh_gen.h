7c8543f4df07ec3ba20b237f32f307a88e9bef8f
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2024-11-19 15:25:25 +0100

    Update tests_dh_gen.h

--- src/Partie2/tests_dh_gen.h
+++ src/Partie2/tests_dh_gen.h
@@ -10,13 +10,4 @@ void test_option_o_no_filename();
 // Fonction pour tester l'option -h
 void test_option_h();
 
-// Fonction pour vérifier que p est un nombre premier de Sophie Germain
-int is_prime(int n);
-int is_sophie_germain_prime(int p);
-void test_prime_sophie_germain();
-
-// Fonction pour tester que g est un générateur de Z/pZ
-int is_generator(int g, int p);
-void test_generator();
-
 #endif // DH_GEN_GROUP_TEST_H
