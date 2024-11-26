3e8a66cc2a2abdf7a34e93dc0f39d1f95162b804
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2024-11-26 15:25:32 +0100

    Pour demande d'aide/bientôt fini ^^
    Please kylian. aide moi avec ces path fles ... j'y comprends pas grand_chose

---
+++ src/Partie2/tests_dh_genA.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "tests_dh_gen.h"
+
+// Macro d'assertion pour afficher les erreurs spécifiques
+#define assert_message(condition, message) \
+    if (!(condition)) { \
+        fprintf(stderr, "Echec de l'assertion : %s\nFichier : %s, Ligne : %d\n", message, __FILE__, __LINE__); \
+        exit(EXIT_FAILURE); \
+    }
+
+// Fonction de test pour l'option -o sans nom de fichier
+void test_option_o_no_filename() {
+    int result = system("./dh_gen_group -o > /dev/null"); // "> /dev/null" pour ne pas afficher la sortie de dh_gen_group durant le test
+    assert_message(result != 0, "L'option -o sans nom de fichier aurait dû échouer");
+    printf("Test de l'option -o sans nom de fichier réussi.\n");
+}
+
+// Fonction de test pour l'option -o avec nom de fichier valide
+void test_option_o() {
+    int result = system("./dh_gen_group -o param.txt");
+    assert_message(result == 0, "L'option -o avec nom de fichier valide a échoué");
+    
+    FILE *file = fopen("tmp/param.txt", "r");
+    assert_message(file != NULL, "Fichier param.txt non créé avec l'option -o");
+    fclose(file);
+    printf("Test de l'option -o avec nom de fichier valide réussi.\n");
+}
+
+// Fonction de test pour l'option -h
+void test_option_h() {
+    int result = system("./dh_gen_group -h > /dev/null"); // "> /dev/null" pour ne pas afficher la sortie de dh_gen_group durant le test
+    assert_message(result == 0, "L'option -h aurait dû afficher l'aide sans erreur");
+    printf("Test de l'option -h réussi.\n");
+}
+
+// L'ensemble des fonctions du fichiers dh_prime qu'on utilise sont déjà vérifié, rabin, test_prime...
