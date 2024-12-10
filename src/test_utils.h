8712fcdec69340fe4a0a79f0595dd83f41a10d53
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2024-12-10 15:08:43 +0100

    Update test_utils.h

--- src/test_utils.h
+++ src/test_utils.h
@@ -1,6 +1,11 @@
 #ifndef TEST_UTILS_H
 #define TEST_UTILS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 /**
  * @brief Macro pour afficher des messages d'erreur et arrêter le programme en cas d'échec d'une assertion.
  */
@@ -10,4 +15,20 @@
         exit(EXIT_FAILURE); \
     }
 
-#endif
+/**
+ * @brief Crée un fichier fictif pour les tests.
+ * 
+ * @param filename Nom du fichier à créer.
+ * @param content Contenu à écrire dans le fichier.
+ */
+void create_mock_file(const char* filename, const char* content);
+
+/**
+ * @brief Nettoie un ensemble de fichiers en les supprimant.
+ * 
+ * @param files Tableau de noms de fichiers.
+ * @param count Nombre de fichiers dans le tableau.
+ */
+void cleanup_files(const char* files[], int count);
+
+#endif // TEST_UTILS_H
